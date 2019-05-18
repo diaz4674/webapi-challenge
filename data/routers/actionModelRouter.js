be7630a8d0ede4c76b1886861eddfb97d9cfceea
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-05-18 10:03:07 -0700

    working localhost

---
+++ data/routers/actionModelRouter.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const Action = require('../helpers/actionModel.js');
+const router = express.Router();
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const actionRouter = require('./data/routers/actionModelRouter')
+const projectRouter = require('./data/routers/projectModelRouter')
+
+
+server.use(express.json())
+// Routes
+server.use('/api/actions', actionRouter)
+server.use('/api/projects', projectRouter)
+
+server.get("/", (req, res) =>{
+    res.send( "hello")
+})
+
+
+
+
+
+module.exports = server; 
